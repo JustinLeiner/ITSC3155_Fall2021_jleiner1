875cc93a2e2142b553ee3ad4fe6e3e2fd5ce2f2b
JustinLeiner <jleiner1@uncc.edu>
2021-11-16 11:28:14 -0500

    added registration functionality

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,21 +1,33 @@
 from database import db
+import datetime
 
-class Note(db.model):
+class Note(db.Model):
     id = db.Column("id", db.Integer, primary_key = True)
-    title = db.Column("titile", db.string(200))
-    text = db.Column("text", db.string(100))
-    date = db.Column("date", db.string(50))
+    title = db.Column("titile", db.String(200))
+    text = db.Column("text", db.String(100))
+    date = db.Column("date", db.String(50))
 
-    def __init__(self, title, text, date):
+    #can create a foreign key, refrences the id variable in the User class (hints why it is lowercase)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, title, text, date, user_id):
         self.title = title
         self.text = text
         self.date = date
+        self.user_id = user_id
 
-class User(db.model):
-    id = db.Column("id", db.Integer, primary_key = True)
-    name = db.Column("name", db.String(100))
+class User(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    first_name = db.Column("first_name", db.String(100))
+    last_name = db.Column("last_name", db.String(100))
     email = db.Column("email", db.String(100))
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    notes = db.relationship("Note", backref="user", lazy=True)
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email 
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.date.today()
