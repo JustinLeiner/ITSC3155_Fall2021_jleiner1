e4c5f281c188bc4986ceb96b07ef386514d7a68a
JustinLeiner <jleiner1@uncc.edu>
2021-11-16 12:16:14 -0500

    added logout functionality and updated user navigation links

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -189,6 +189,14 @@ def login():
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
+@app.route('/logout')
+def logout():
+    # check if a user is saved in session
+    if session.get('user'):
+        session.clear()
+
+    return redirect(url_for('index'))
+
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
 
